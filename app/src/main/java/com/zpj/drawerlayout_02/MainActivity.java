e8e2bd31b93593748efa74a2c280dd4709ab1c8a
zpjgit <zpj.love@foxmail.com>
2017-09-18 11:38:25 +0800

    添加了一张图片作为导航栏按钮让 点击这个按钮也可以打开 滑动时打开的菜单

--- app/src/main/java/com/zpj/drawerlayout_02/MainActivity.java
+++ app/src/main/java/com/zpj/drawerlayout_02/MainActivity.java
@@ -1,13 +1,46 @@
 package com.zpj.drawerlayout_02;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        try {
+            setSupportActionBar(toolbar);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true); //显示导航栏按钮
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+
+            default:
+        }
+//        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
